a3d8d8d6c9f9f97d2304b9b9d8469894021f895f
vitaly1234567890 <Troyanvitaly123@yandex.ru>
2024-03-19 07:19:19 +0300

    fix bags

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -13,7 +13,7 @@ export const Header = (props: HeaderType) => {
         <header className={classes.header}>
             <img src='https://www.legal.hireca.com/uploads/images/image_slider_2564_5a867f9983a54.jpg'/>
             <div className={classes.loginBlock}>
-                {props.isAuth ? <div> {props.login} - <button onClick={props.logout} >Logout</button></div>
+                {props.isAuth ? <div> <p>user: {props.login}</p> <button onClick={props.logout} >Logout</button></div>
                 : <NavLink to={'/login'}>Login</NavLink>}
 
             </div>
